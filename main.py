bc9a9e2a839c29cf768b8e137407f963762ce32b
staysdavid2202-bit <staysdavid2202@gmail.com>
2025-10-12 13:11:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import telebot
 import requests
 import os
-
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
